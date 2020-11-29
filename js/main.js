fa239a5f48ab25733d114c635bc55fed8bdb4de4
Ksenia Kiseleva <69579994+kmkiseleva@users.noreply.github.com>
2020-11-29 20:26:21 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,25 +11,30 @@ const popup = document.getElementById('popup-wrap');
 
 // бургер
 
-// (function () {
-//    const burgerItem = document.querySelector('.burger');
-//    const menu = document.querySelector('.menu');
-//    const menuCloseItem = document.querySelector('.menu__nav-close');
-//    const menuLinks = document.querySelectorAll('.menu-link');
-//    burgerItem.addEventListener('click', () => {
-//       menu.classList.add('menu_active');
-//       burgerItem.style.display = "none";
-//    });
-//    menuCloseItem.addEventListener('click', () => {
-//       menu.classList.remove('menu_active');
-//       if (window.innerWidth < 1200) {
-//          burgerItem.style.display = "block";
-//       };
-//    });
-// }());
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+  const menuCloseItem = document.querySelector('.menu__nav-close');
+  const menuLinks = document.querySelectorAll('.menu__link');
+  const trends = document.querySelector('.main-trends');
+  const content = document.querySelector('.content');
+  burgerItem.addEventListener('click', () => {
+     menu.classList.add('menu_active');
+     content.style.display = "none";
+     trends.style.display = "none";
 
-// (function () {
-//    if (window.innerWidth >= 1200) {
-//       burgerItem.style.display = "none";
-//    };
-// }());
+  });
+  menuCloseItem.addEventListener('click', () => {
+     menu.classList.remove('menu_active');
+     content.style.display = "block";
+     trends.style.display = "block";
+  });
+
+  if (window.innerWidth < 1200) {
+     for (let i = 0; i < menuLinks.length; i++) {
+        menuLinks[i].addEventListener('click', () => {
+           menu.classList.remove('menu_active');
+        })
+     }
+  };
+}());
